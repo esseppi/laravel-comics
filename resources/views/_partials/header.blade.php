4cfb1e560beea50194c3c3804d200d5d0312069f
Your Name <you@example.com>
2022-04-27 16:26:17 +0200

    header e structure

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="top-header">
+        <div class="container">
+            <h4>DC POWER VISA</h4>
+            <h4>ADDITIONAL DC SITES</h4>
+        </div>
+    </div>
+    <nav class="navigator">
+        <div class="container row">
+            <div class="">
+                <img src="./images/dc-logo.png" alt="">
+            </div>
+
+            <ul class="menu-list">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+
+            <div class="search">
+                <input type="text">
+            </div>
+        </div>
+    </nav>
+</header>
